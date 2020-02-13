f78efc10c35c19cd410f4b3baceb84f61f1933de
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-02-13 23:26:02 +0100

    Refactored HelperInit

--- anchor-bean/src/main/java/org/anchoranalysis/bean/init/HelperInit.java
+++ anchor-bean/src/main/java/org/anchoranalysis/bean/init/HelperInit.java
@@ -1,6 +1,6 @@
 package org.anchoranalysis.bean.init;
 
-import java.lang.reflect.Field;
+
 
 /*
  * #%L
@@ -30,14 +30,11 @@ import java.lang.reflect.Field;
 
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.anchoranalysis.bean.AnchorBean;
-import org.anchoranalysis.bean.annotation.Optional;
-import org.anchoranalysis.bean.annotation.SkipInit;
 import org.anchoranalysis.bean.init.property.PropertyInitializer;
 import org.anchoranalysis.core.error.InitException;
 import org.anchoranalysis.core.log.LogErrorReporter;
@@ -80,81 +77,27 @@ class HelperInit {
 
 	}
 	
-	private static void maybeInitChildren( BeanAndParent beanAndParent, List<BeanAndParent> listInit, String beanNameFollowingFrom, PropertyInitializer<?> pi, LogErrorReporter logger ) throws InitException {
+	private static void maybeInitChildren( BeanAndParent bean, List<BeanAndParent> listInit, String beanNameFollowingFrom, PropertyInitializer<?> pi, LogErrorReporter logger ) throws InitException {
 		
 		try {
-			boolean didInitChild = pi.execIfInheritsFrom(beanAndParent.getBean(), beanAndParent.parentBean(), logger); 
+			boolean didInitChild = pi.execIfInheritsFrom(bean.getBean(), bean.parentBean(), logger); 
 			if (didInitChild) {
-				// Add children of the object, but only if PropertyInitializer initialize the object
-				addChildrenForInit(listInit, beanAndParent);		
+				// Add children of the object, but only if PropertyInitializer initializes the object
+				FindChildrenForInit.addChildrenFromBean(bean, listInit);		
 			} else {
-				throwExceptionIfInitializable( beanAndParent.getBean(), beanNameFollowingFrom, pi.getInitParamType().toString() );
+				throwExceptionIfInitializable( bean.getBean(), beanNameFollowingFrom, pi.getInitParamType().toString() );
 			}
-		
 		
 		} catch (InitException e) {
 			String msg = String.format(
 				"Cannot initialize a field '%s' when initializing '%s'",
-				beanAndParent.pathFromRootAsString(),
+				bean.pathFromRootAsString(),
 				beanNameFollowingFrom
 			);
 			throw new InitException(msg,e);
 		}
 	}
-	
-	
-	private static void addChildrenForInit( List<BeanAndParent> listInit, BeanAndParent parentBean ) throws InitException {
 		
-		List<Field> beanFields = parentBean.getBean().getOrCreateBeanFields();
-		
-		for(Field field  : beanFields) {
-			
-			if (!field.isAnnotationPresent(SkipInit.class)) {
-			
-				// Create a FIFO queue of items to be initialised
-				addChildrenMany( listInit, parentBean, field, field.isAnnotationPresent(Optional.class) );
-			}
-		}
-	}
-	
-	private static void addChildrenMany( List<BeanAndParent> listInit, BeanAndParent parentBean, Field field, boolean optional ) throws InitException {
-	 
-		try {
-			Object propertyValue = field.get(parentBean.getBean());
-			
-			 // If it's a collection, then we do it item by item in the collection, and then exit
-			 if (propertyValue instanceof Collection) {
-				 
-				 Collection<?> collection = (Collection<?>) propertyValue;
-				 for( Object o : collection ) {
-					 addChildren( listInit, parentBean, o, false );
-				 }
-				 
-				 return;
-	         }
-			
-			addChildren( listInit, parentBean, propertyValue, optional );
-			
-		} catch (IllegalArgumentException | IllegalAccessException e) {
-			throw new InitException(e);
-		}
-	}
-	
-	private static void addChildren( List<BeanAndParent> listInit, BeanAndParent parentBean, Object propertyValue, boolean optional ) throws InitException {
-		
-		// If it's an optional attribute we check that it's not null
-    	if (optional && propertyValue==null) {
-   			 return;
-    	}
-    	
-    	// We only add if its a bean itself
-    	if (propertyValue instanceof AnchorBean) {
-    		listInit.add(
-    			new BeanAndParent( (AnchorBean<?>) propertyValue, parentBean )
-    		);
-    	}
-	}
-	
 	private static void throwExceptionIfInitializable( AnchorBean<?> bean, String beanNameFollowingFrom, String provider ) throws InitException {
 		
 		if (bean instanceof InitializableBean) {
