41f7ad073a44fb303c2e4a4e9e2a3eec9e19bc16
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-03-01 23:31:54 +0100

    anchoranalysis/anchor#20 made day optional on automatic experiment-identifier

--- anchor-experiment/src/main/java/org/anchoranalysis/experiment/bean/identifier/ExperimentIdentifierAuto.java
+++ anchor-experiment/src/main/java/org/anchoranalysis/experiment/bean/identifier/ExperimentIdentifierAuto.java
@@ -50,9 +50,23 @@ public class ExperimentIdentifierAuto extends ExperimentIdentifier {
 	/** If there's no task-name, then this constant is used as a fallback name */
 	@BeanField
 	private String fallbackName = "experiment";
-	// END BEAN FIELDS
 	
-	private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy.MM.dd.HH.mm");
+	/** If true, the current day (yyyy.MM.dd) is included in the output */
+	@BeanField
+	private boolean day = true;
+	
+	/** If true, the current time (HH.mm.ss) is included in the output */
+	@BeanField
+	private boolean time = true;
+	
+	/** Only relevant if time==true. Whether the second is included in the time */
+	@BeanField
+	private boolean second = true;
+	// END BEAN FIELDS
+
+	private static String FORMAT_DAY = "yyyy.MM.dd";
+	private static String FORMAT_TIME_BASE = "HH.mm";
+	private static String FORMAT_TIME_SECOND = "ss";
 	
 	@Override
 	public String identifier(String taskName) {
@@ -61,7 +75,13 @@ public class ExperimentIdentifierAuto extends ExperimentIdentifier {
 			determineVersion()
 		);
 	}
-
+		
+	/**
+	 * Calculates the experiment-name part of the identifier
+	 * 
+	 * @param taskName if non-null a string describing the current task, or null if none exists
+	 * @return the experiment-name
+	 */
 	private String determineExperimentName(String taskName) {
 		if (taskName!=null) {
 			return removeSpecialChars(
@@ -72,8 +92,54 @@ public class ExperimentIdentifierAuto extends ExperimentIdentifier {
 		}
 	}
 	
+	/**
+	 * Calculates the version part of the identifier
+	 * 
+	 * @return a string describing the version or null if version should be omitted
+	 */
 	private String determineVersion() {
-		return dtf.format( LocalDateTime.now() );
+		DateTimeFormatter dtf = createFormatter();
+		if (dtf!=null) {
+			return dtf.format( LocalDateTime.now() );
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Creates a date-time formatter
+	 * 
+	 * @return the formatter or null if neither the day or time should be included
+	 */
+	private DateTimeFormatter createFormatter() {
+		String formatterStr = createFormatterPatternStr();
+		if (formatterStr!=null) {
+			return DateTimeFormatter.ofPattern(formatterStr);
+		} else {
+			return null;
+		}
+	}
+	
+	private String createFormatterPatternStr() {
+		if (day) {
+			if (time) {
+				return FORMAT_DAY + "." + createPatternStrTime();
+			} else {
+				return FORMAT_DAY;
+			}
+		} else if (time) {
+			return createPatternStrTime();
+		} else {
+			return null;
+		}
+	}
+	
+	private String createPatternStrTime() {
+		if (second) {
+			return FORMAT_TIME_BASE + "." + FORMAT_TIME_SECOND;
+		} else {
+			return FORMAT_TIME_BASE;
+		}
 	}
 	
 	private static String finalPartOfTaskName( String taskName ) {
@@ -84,4 +150,38 @@ public class ExperimentIdentifierAuto extends ExperimentIdentifier {
 	private static String removeSpecialChars(String str) {
 		return str.replaceAll("[^a-zA-Z]+","");
 	}
+
+	public String getFallbackName() {
+		return fallbackName;
+	}
+
+	public void setFallbackName(String fallbackName) {
+		this.fallbackName = fallbackName;
+	}
+
+	public boolean isDay() {
+		return day;
+	}
+
+	public void setDay(boolean day) {
+		this.day = day;
+	}
+
+	public boolean isTime() {
+		return time;
+	}
+
+	public void setTime(boolean time) {
+		this.time = time;
+	}
+
+	public boolean isSecond() {
+		return second;
+	}
+
+	public void setSecond(boolean second) {
+		this.second = second;
+	}
+
+
 }
