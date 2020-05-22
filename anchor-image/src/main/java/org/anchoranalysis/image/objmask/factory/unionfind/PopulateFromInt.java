c54f4debe0ffb332d4979d994aa361cd1c469a4a
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-05-22 18:49:40 +0200

    Removed left-over code from PopulateFromInt

--- anchor-image/src/main/java/org/anchoranalysis/image/objmask/factory/unionfind/PopulateFromInt.java
+++ anchor-image/src/main/java/org/anchoranalysis/image/objmask/factory/unionfind/PopulateFromInt.java
@@ -40,65 +40,6 @@ import org.jgrapht.alg.util.UnionFind;
 final class PopulateFromInt extends PopulateIndexFromBinary<IntBuffer> {
 
 	@Override
-	public int populateIndexFromBinary(
-		BinaryVoxelBox<IntBuffer> visited,
-		VoxelBox<IntBuffer> indexBuffer,
-		SlidingBuffer<IntBuffer> slidingIndex,
-		UnionFind<Integer> unionIndex,
-		boolean bigNghb
-	) {
-		boolean do3D = indexBuffer.extnt().getZ()>1;
-		
-		MergeWithNghbs mergeWithNgbs = new MergeWithNghbs(slidingIndex, unionIndex, do3D, bigNghb);
-		
-		int cnt = 1;
-		BinaryValues bv = visited.getBinaryValues();
-		Extent extnt = visited.extnt();
-		for (int z=0; z<extnt.getZ(); z++) {
-			
-			IntBuffer bbBinary = visited.getVoxelBox().getPixelsForPlane(z).buffer();
-			IntBuffer bbIndex = indexBuffer.getPixelsForPlane(z).buffer();
-			
-			cnt = processSlice(extnt, bbBinary, bbIndex, bv, mergeWithNgbs, z, cnt, unionIndex);
-						
-			slidingIndex.shift();
-		}
-		return cnt-1;
-
-	}
-		
-	private int processSlice(Extent extnt, IntBuffer bbBinary, IntBuffer bbIndex, BinaryValues bv, MergeWithNghbs mergeWithNgbs, int z, int cnt, UnionFind<Integer> unionIndex) {
-
-		int offset = 0;
-		
-		for (int y=0; y<extnt.getY(); y++) {
-			for (int x=0; x<extnt.getX(); x++) {
-				
-				if (bbBinary.get(offset)==bv.getOnInt() && bbIndex.get(offset)==0) {
-					
-					int nghbLab = mergeWithNgbs.calcMinNghbLabel(
-						new Point3i(x, y, z),
-						offset
-					);
-					if (nghbLab==-1) {
-						bbBinary.put(offset, cnt );
-						bbIndex.put(offset, cnt);
-						unionIndex.addElement(cnt);
-						cnt++;
-					} else {
-						bbIndex.put(offset, nghbLab);
-					}
-				}
-				
-				offset++;
-			}
-			
-		}
-		
-		return cnt;
-	}
-
-	@Override
 	protected boolean isBufferOn(IntBuffer buffer, int offset, BinaryValues bv, BinaryValuesByte bvb) {
 		return buffer.get(offset)==bv.getOnInt();
 	}
