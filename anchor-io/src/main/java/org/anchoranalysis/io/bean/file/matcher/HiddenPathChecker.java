84e00b005e8dc998e52e7af1882cc0dd9eeb622a
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-05-25 22:40:26 +0200

    Trying to fix a bug that appears in the Linux subsystem of Windows 10

--- anchor-io/src/main/java/org/anchoranalysis/io/bean/file/matcher/HiddenPathChecker.java
+++ anchor-io/src/main/java/org/anchoranalysis/io/bean/file/matcher/HiddenPathChecker.java
@@ -36,24 +36,25 @@ import org.apache.commons.lang.SystemUtils;
 
 class HiddenPathChecker {
 	
-	public static boolean includePath( Path path ) {
-		
+	public static boolean includePath(Path path) {
 		try {
 			// There is a bug in Java (apparently fixed in version 13) where Files.isHidden
 			//  does not recognize directories as hidden.
 			return !Files.exists(path) || !isHidden(path);
-		} catch (IOException e) {
+		} catch (Exception e) {
 			// If we can't perform these operations, we consider the file not to be hidden
 			// rather than throwing an exception
 			return true;
 		}
 	}
 	
-	// A workaround for a bug in Java (apparently fixed in version 13) where Files.isHidden
-	//  does not recognise directories as being hidden.
-	// https://stackoverflow.com/questions/53791740/why-does-files-ishiddenpath-return-false-for-directories-on-windows
+	/* 
+	 * A workaround for a bug in Java (apparently fixed in version 13) where {@link Files.isHidden}
+	 *  does not recognise directories as being hidden.
+	 *  
+	 * <p>See <a href="https://stackoverflow.com/questions/53791740/why-does-files-ishiddenpath-return-false-for-directories-on-windows">Stack Overflow</a></p>
+	 **/
 	private static boolean isHidden(Path path) throws IOException {
-		
 		try {
 			if (SystemUtils.IS_OS_WINDOWS) {
 				DosFileAttributes dosFileAttributes = Files.readAttributes(
@@ -63,6 +64,8 @@ class HiddenPathChecker {
 				);
 				return dosFileAttributes.isHidden();
 			} else {
+				// Note a {@link ArrayIndexOutOfBoundsException} is being thrown here when running on
+				// the Linux subsystem of Windows. It's caught in {@link includePath}.
 				return Files.isHidden(path);
 			}
 		} catch (UnsupportedOperationException e) {
