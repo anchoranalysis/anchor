fd7525d5ceb0afdf7bca74a2b41ed43238d60e71
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-07-13 11:35:51 +0200

    Added functional-utilities for ToIntFunctionWithException.java and related

--- anchor-core/src/main/java/org/anchoranalysis/core/functional/FunctionalUtilities.java
+++ anchor-core/src/main/java/org/anchoranalysis/core/functional/FunctionalUtilities.java
@@ -70,10 +70,10 @@ public class FunctionalUtilities {
 	/**
 	 * Performs a map on a stream, but accepts a function that can throw a checked-exception
 	 * 
-	 * <p>This uses some internal reflection trickery to suppress the checked exception, and then rethrow it.</p>
-	 * 
-	 * <p>As a side-effect, any runtime exceptions that are thrown during the function, will be rethrown
-	 * wrapped inside a {@link ConvertedToRuntimeException}</p>
+	 * This uses some internal reflection trickery to suppress the checked exception, and then rethrow it.
+	 * <p>
+	 * As a side-effect, any runtime exceptions that are thrown during the function, will be rethrown
+	 * wrapped inside a {@link ConvertedToRuntimeException}.
 	 * 
 	 * @param  <S> input-type to map
 	 * @param  <T> output-type of map
@@ -91,6 +91,37 @@ public class FunctionalUtilities {
 	) throws E {
 		try {
 			return stream.map( item-> 
+				suppressCheckedException(item, mapFunction)
+			);
+			
+		} catch (ConvertedToRuntimeException e) {
+			return throwException(e, throwableClass);
+		}
+	}
+	
+	/**
+	 * Performs a {@code mapToInt} on a stream, but accepts a function that can throw a checked-exception.
+	 * <p>
+	 * This uses some internal reflection trickery to suppress the checked exception, and then rethrow it.
+	 * <p>
+	 * As a side-effect, any runtime exceptions that are thrown during the function, will be rethrown
+	 * wrapped inside a {@link ConvertedToRuntimeException}.
+	 * 
+	 * @param  <S> input-type to map
+	 * @param  <E> exception that can be thrown by {code mapFunction}
+	 * @param  stream the stream to apply the map on
+	 * @param  throwableClass the class of {@code E}
+	 * @param  mapFunction the function to use for mapping
+	 * @return the output of the flatMap
+	 * @throws E if the exception is thrown during mapping
+	 */
+	public static <S,E extends Exception> IntStream mapToIntWithException(
+		Stream<S> stream,
+		Class<? extends Exception> throwableClass,
+		ToIntFunctionWithException<S,E> mapFunction
+	) throws E {
+		try {
+			return stream.mapToInt( item-> 
 				suppressCheckedException(item, mapFunction)
 			);
 			
@@ -239,58 +270,6 @@ public class FunctionalUtilities {
 		}
 	}
 	
-	@SuppressWarnings("unchecked")
-	/**
-	 * Rethrows either the cause of a run-time exception (as a checked exception) or the run-time exception itself, depending if the cause matches the expected type. 
-	 * 
-	 * @param <T> return-type (nothing ever returned, this is just to keep types compatible in a nice way)
-	 * @param <E> the exception type that may be the "cause" of the {@link ConvertedToRuntimeException}, in which case, it would be rethrown
-	 * @param e the exception, which will be either rethrown as-is, or its cause will be rethrown.
-	 * @param throwableClass the class of {@code E}
-	 * @return nothing, as an exception will always be thrown
-	 * @throws E always, rethrowing either the run-time exception or its cause.
-	 */
-	private static <T, E extends Exception> T throwException(ConvertedToRuntimeException e, Class<? extends Exception> throwableClass) throws E {
-		if (throwableClass.isAssignableFrom(e.getException().getClass())) {
-			throw (E) e.getException();	
-		} else {
-			throw e;
-		}
-	}
-	
-	/**
-	 * Catches any exceptions that occur around a function as it is executed and wraps them into a run-time exception.
-	 * 
-	 * @param <S> parameter-type for function
-	 * @param <T> return-type for function
-	 * @param <E> checked-exception that can be thrown by function
-	 * @param param the parameter to apply to the function
-	 * @param function the function
-	 * @return the return-value of the function
-	 * @throws ConvertedToRuntimeException a run-time exception if an exception is thrown by {@link function}
-	 */
-	private static <S,T,E extends Exception> T suppressCheckedException(
-		S param,
-		FunctionWithException<S,T,E> function
-	) {
-		try {
-			return function.apply(param);
-		} catch (Exception exc) {
-			throw new ConvertedToRuntimeException(exc);
-		}
-	}
-	
-	private static <T,E extends Exception> T suppressCheckedException(
-		int param,
-		IntFunctionWithException<T,E> function
-	) {
-		try {
-			return function.apply(param);
-		} catch (Exception exc) {
-			throw new ConvertedToRuntimeException(exc);
-		}
-	}
-	
 	/**
 	 * Maps a collection to a list with each element derived from a corresponding element in the original collection.
 	 * <p>
@@ -366,5 +345,70 @@ public class FunctionalUtilities {
 			throwableClass,
 			mapFunction
 		).collect( Collectors.toList() );
+	}
+		
+	@SuppressWarnings("unchecked")
+	/**
+	 * Rethrows either the cause of a run-time exception (as a checked exception) or the run-time exception itself, depending if the cause matches the expected type. 
+	 * 
+	 * @param <T> return-type (nothing ever returned, this is just to keep types compatible in a nice way)
+	 * @param <E> the exception type that may be the "cause" of the {@link ConvertedToRuntimeException}, in which case, it would be rethrown
+	 * @param e the exception, which will be either rethrown as-is, or its cause will be rethrown.
+	 * @param throwableClass the class of {@code E}
+	 * @return nothing, as an exception will always be thrown
+	 * @throws E always, rethrowing either the run-time exception or its cause.
+	 */
+	private static <T, E extends Exception> T throwException(ConvertedToRuntimeException e, Class<? extends Exception> throwableClass) throws E {
+		if (throwableClass.isAssignableFrom(e.getException().getClass())) {
+			throw (E) e.getException();	
+		} else {
+			throw e;
+		}
+	}
+	
+	/**
+	 * Catches any exceptions that occur around a function as it is executed and wraps them into a run-time exception.
+	 * 
+	 * @param <S> parameter-type for function
+	 * @param <T> return-type for function
+	 * @param <E> checked-exception that can be thrown by function
+	 * @param param the parameter to apply to the function
+	 * @param function the function
+	 * @return the return-value of the function
+	 * @throws ConvertedToRuntimeException a run-time exception if an exception is thrown by {@link function}
+	 */
+	private static <S,T,E extends Exception> T suppressCheckedException(
+		S param,
+		FunctionWithException<S,T,E> function
+	) {
+		try {
+			return function.apply(param);
+		} catch (Exception exc) {
+			throw new ConvertedToRuntimeException(exc);
+		}
+	}
+	
+	/** Like @link(#suppressCheckedException) but instead accepts {@link ToIntFunctionWithException} functions */
+	private static <S,E extends Exception> int suppressCheckedException(
+		S param,
+		ToIntFunctionWithException<S,E> function
+	) {
+		try {
+			return function.apply(param);
+		} catch (Exception exc) {
+			throw new ConvertedToRuntimeException(exc);
+		}
+	}
+	
+	/** Like @link(#suppressCheckedException) but instead accepts {@link IntFunctionWithException} functions */
+	private static <T,E extends Exception> T suppressCheckedException(
+		int param,
+		IntFunctionWithException<T,E> function
+	) {
+		try {
+			return function.apply(param);
+		} catch (Exception exc) {
+			throw new ConvertedToRuntimeException(exc);
+		}
 	}
 }
