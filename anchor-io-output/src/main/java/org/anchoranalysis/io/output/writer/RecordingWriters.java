af809c65cf34e01b04e26f7b091b30f2c5818cad
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-09-24 18:43:34 +0200

    Fixed javadoc bug.

--- anchor-io-output/src/main/java/org/anchoranalysis/io/output/writer/RecordingWriters.java
+++ anchor-io-output/src/main/java/org/anchoranalysis/io/output/writer/RecordingWriters.java
@@ -11,6 +11,7 @@ import lombok.experimental.Accessors;
  * <ul>
  * <li>A {@link CheckIfAllowed} that selectively outputs certain output-names.
  * <li>A {@link AlwaysAllowed} that allows all output-names.
+ * </ul>
  * 
  * @author Owen Feehan
  */
