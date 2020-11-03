50e6d8c7dfe446e0d4320388b6c6a5fc1aefbdac
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-11-03 11:52:53 +0100

    anchoranalysis/anchor#88 added tests to CommonPathTest

--- anchor-core/src/test/java/org/anchoranalysis/core/system/path/CommonPathTest.java
+++ anchor-core/src/test/java/org/anchoranalysis/core/system/path/CommonPathTest.java
@@ -9,12 +9,35 @@ import java.util.List;
 import java.util.Optional;
 import org.junit.Test;
 
+/**
+ * Tests {@link CommonPath}.
+ * 
+ * @author Owen Feehan
+ *
+ */
 public class CommonPathTest {
 
+    /** Two absolute-paths with commonality. */
     @Test
-    public void testDirectoryAbsoluteWindowsDirectory() throws IOException {
-        List<File> files = Arrays.asList( file("/a/b/c/d"), file("/a/b") );
-        assertEquals( Optional.of(Paths.get("/a/b")), CommonPath.commonPath(files) );
+    public void testDirectoryAbsolutePath() throws IOException {
+        assertCommonPath( "/a/b/c/d",  "/a/b", Optional.of("/a/b"));
+    }
+
+    /** Two relative-paths with commonality. */
+    @Test
+    public void testDirectoryRelativePath() throws IOException {
+        assertCommonPath( "a/b/c/d",  "a/b", Optional.of("a/b"));
+    }
+
+    /** One absolute path and one relative-path - with therefore no commonality. */
+    @Test
+    public void testDirectoryAbsoluteAndRelativePath() throws IOException {
+        assertCommonPath( "/a/b/c/d",  "a/b", Optional.empty());
+    }
+    
+    private static void assertCommonPath(String path1, String path2, Optional<String> expectedCommonPath) {
+        List<File> files = Arrays.asList( file(path1), file(path2) );
+        assertEquals( expectedCommonPath.map(Paths::get), CommonPath.commonPath(files) );
     }
     
     private static File file(String path) {
